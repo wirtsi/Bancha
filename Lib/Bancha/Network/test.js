7f63467edb155a5e806f7ab0a0d650a62d252ace
wirtsi <spam_for_flow@gmx.de>
2012-02-21 15:16:23 +0100

    merge upstream

---
+++ Lib/Bancha/Network/test.js
@@ -0,0 +1,196 @@
+[
+    {"type":"rpc", "tid":1, "action":"Director", "method":"read", "result":{"success":true, "data":[
+        {"Director":{"id":"1", "name":"Francis Ford Coppola", "alive":true}},
+        {"Director":{"id":"2", "name":"Oliver Stone", "alive":true}},
+        {"Director":{"id":"5", "name":"Danny Boyle", "alive":true}},
+        {"Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}},
+        {"Director":{"id":"12", "name":"Christopher Nolan", "alive":true}},
+        {"Director":{"id":"13", "name":"Mary Harron", "alive":true}},
+        {"Director":{"id":"14", "name":"Mr Frimp", "alive":true}}
+    ], "total":7}},
+    {"type":"rpc", "tid":2, "action":"Actor", "method":"read", "result":{"success":true, "data":[
+        {"Actor":{"id":"2", "name":"Marlon Brando", "movie_id":"1"}},
+        {"Actor":{"id":"3", "name":"Mr. T", "movie_id":"1"}},
+        {"Actor":{"id":"1", "name":"Dennis Hopper", "movie_id":"2"}},
+        {"Actor":{"id":"2", "name":"Marlon Brando", "movie_id":"2"}},
+        {"Actor":{"id":"3", "name":"Mr. T", "movie_id":"2"}},
+        {"Actor":{"id":"1", "name":"Dennis Hopper", "movie_id":"3"}},
+        {"Actor":{"id":"2", "name":"Marlon Brando", "movie_id":"3"}},
+        {"Actor":{"id":"9", "name":"Christian Bale", "movie_id":"4"}},
+        {"Actor":{"id":"10", "name":"Heath Ledger", "movie_id":"4"}},
+        {"Actor":{"id":"9", "name":"Christian Bale", "movie_id":"5"}},
+        {"Actor":{"id":"13", "name":"Josh Lucas", "movie_id":"5"}},
+        {"Actor":{"id":"11", "name":"Tim Roth", "movie_id":"6"}},
+        {"Actor":{"id":"12", "name":"Bruno Glanz", "movie_id":"6"}},
+        {"Actor":{"id":"2", "name":"Marlon Brando", "movie_id":"7"}},
+        {"Actor":{"id":"14", "name":"Al Pacino", "movie_id":"7"}},
+        {"Actor":{"id":"14", "name":"Al Pacino", "movie_id":"8"}},
+        {"Actor":{"id":"15", "name":"Robert De Niro", "movie_id":"8"}},
+        {"Actor":{"id":"14", "name":"Al Pacino", "movie_id":"9"}},
+        {"Actor":{"id":"16", "name":"Andy Garcia", "movie_id":"9"}},
+        {"Actor":{"id":"20", "name":"Keanu Reeves", "movie_id":"10"}},
+        {"Actor":{"id":"17", "name":"Val Kilmer", "movie_id":"11"}},
+        {"Actor":{"id":"18", "name":"Meg Ryan", "movie_id":"11"}},
+        {"Actor":{"id":"19", "name":"Tom Cruise", "movie_id":"12"}},
+        {"Actor":{"id":"14", "name":"Al Pacino", "movie_id":"13"}},
+        {"Actor":{"id":"22", "name":"Cameron Diaz", "movie_id":"13"}},
+        {"Actor":{"id":"21", "name":"Naomie Harris", "movie_id":"14"}},
+        {"Actor":{"id":"23", "name":"Leonardo DiCaprio", "movie_id":"15"}},
+        {"Actor":{"id":"24", "name":"Ewan McGregor", "movie_id":"16"}},
+        {"Actor":{"id":"25", "name":"Kurt Russel", "movie_id":"17"}},
+        {"Actor":{"id":"26", "name":"Bruce Willis", "movie_id":"18"}},
+        {"Actor":{"id":"27", "name":"Brad Pitt", "movie_id":"19"}},
+        {"Actor":{"id":"28", "name":"Willem Dafoe", "movie_id":"20"}},
+        {"Actor":{"id":"29", "name":"Charlie Sheen", "movie_id":"20"}},
+        {"Actor":{"id":"30", "name":"Uma Thurman", "movie_id":"21"}},
+        {"Actor":{"id":"31", "name":"Samuel Jackson", "movie_id":"22"}},
+        {"Actor":{"id":"11", "name":"Tim Roth", "movie_id":"23"}},
+        {"Actor":{"id":"32", "name":"Tim Roth", "movie_id":"23"}},
+        {"Actor":{"id":"11", "name":"Tim Roth", "movie_id":"24"}},
+        {"Actor":{"id":"26", "name":"Bruce Willis", "movie_id":"24"}},
+        {"Actor":{"id":"30", "name":"Uma Thurman", "movie_id":"24"}},
+        {"Actor":{"id":"31", "name":"Samuel Jackson", "movie_id":"24"}},
+        {"Actor":{"id":"11", "name":"Tim Roth", "movie_id":"25"}},
+        {"Actor":{"id":"32", "name":"Tim Roth", "movie_id":"25"}}
+    ], "total":43}},
+    {"type":"rpc", "tid":3, "action":"Actor", "method":"read", "result":{"success":true, "data":[
+        {"Actor":{"id":"2", "name":"Marlon Brando", "movie_id":"1"}},
+        {"Actor":{"id":"3", "name":"Mr. T", "movie_id":"1"}},
+        {"Actor":{"id":"1", "name":"Dennis Hopper", "movie_id":"2"}},
+        {"Actor":{"id":"2", "name":"Marlon Brando", "movie_id":"2"}},
+        {"Actor":{"id":"3", "name":"Mr. T", "movie_id":"2"}},
+        {"Actor":{"id":"1", "name":"Dennis Hopper", "movie_id":"3"}},
+        {"Actor":{"id":"2", "name":"Marlon Brando", "movie_id":"3"}},
+        {"Actor":{"id":"9", "name":"Christian Bale", "movie_id":"4"}},
+        {"Actor":{"id":"10", "name":"Heath Ledger", "movie_id":"4"}},
+        {"Actor":{"id":"9", "name":"Christian Bale", "movie_id":"5"}},
+        {"Actor":{"id":"13", "name":"Josh Lucas", "movie_id":"5"}},
+        {"Actor":{"id":"11", "name":"Tim Roth", "movie_id":"6"}},
+        {"Actor":{"id":"12", "name":"Bruno Glanz", "movie_id":"6"}},
+        {"Actor":{"id":"2", "name":"Marlon Brando", "movie_id":"7"}},
+        {"Actor":{"id":"14", "name":"Al Pacino", "movie_id":"7"}},
+        {"Actor":{"id":"14", "name":"Al Pacino", "movie_id":"8"}},
+        {"Actor":{"id":"15", "name":"Robert De Niro", "movie_id":"8"}},
+        {"Actor":{"id":"14", "name":"Al Pacino", "movie_id":"9"}},
+        {"Actor":{"id":"16", "name":"Andy Garcia", "movie_id":"9"}},
+        {"Actor":{"id":"20", "name":"Keanu Reeves", "movie_id":"10"}},
+        {"Actor":{"id":"17", "name":"Val Kilmer", "movie_id":"11"}},
+        {"Actor":{"id":"18", "name":"Meg Ryan", "movie_id":"11"}},
+        {"Actor":{"id":"19", "name":"Tom Cruise", "movie_id":"12"}},
+        {"Actor":{"id":"14", "name":"Al Pacino", "movie_id":"13"}},
+        {"Actor":{"id":"22", "name":"Cameron Diaz", "movie_id":"13"}},
+        {"Actor":{"id":"21", "name":"Naomie Harris", "movie_id":"14"}},
+        {"Actor":{"id":"23", "name":"Leonardo DiCaprio", "movie_id":"15"}},
+        {"Actor":{"id":"24", "name":"Ewan McGregor", "movie_id":"16"}},
+        {"Actor":{"id":"25", "name":"Kurt Russel", "movie_id":"17"}},
+        {"Actor":{"id":"26", "name":"Bruce Willis", "movie_id":"18"}},
+        {"Actor":{"id":"27", "name":"Brad Pitt", "movie_id":"19"}},
+        {"Actor":{"id":"28", "name":"Willem Dafoe", "movie_id":"20"}},
+        {"Actor":{"id":"29", "name":"Charlie Sheen", "movie_id":"20"}},
+        {"Actor":{"id":"30", "name":"Uma Thurman", "movie_id":"21"}},
+        {"Actor":{"id":"31", "name":"Samuel Jackson", "movie_id":"22"}},
+        {"Actor":{"id":"11", "name":"Tim Roth", "movie_id":"23"}},
+        {"Actor":{"id":"32", "name":"Tim Roth", "movie_id":"23"}},
+        {"Actor":{"id":"11", "name":"Tim Roth", "movie_id":"24"}},
+        {"Actor":{"id":"26", "name":"Bruce Willis", "movie_id":"24"}},
+        {"Actor":{"id":"30", "name":"Uma Thurman", "movie_id":"24"}},
+        {"Actor":{"id":"31", "name":"Samuel Jackson", "movie_id":"24"}},
+        {"Actor":{"id":"11", "name":"Tim Roth", "movie_id":"25"}},
+        {"Actor":{"id":"32", "name":"Tim Roth", "movie_id":"25"}}
+    ], "total":43}},
+    {"type":"rpc", "tid":4, "action":"Movie", "method":"read", "result":{"success":true, "data":[
+        {"Movie":{"id":"1", "director_id":"14", "name":"Apocalipse Now", "year":"1979"}, "Director":{"id":"14", "name":"Mr Frimp", "alive":true}, "Actor":[
+            {"id":"2", "name":"Marlon Brando", "MoviesActor":{"id":"82", "movie_id":"1", "actor_id":"2"}},
+            {"id":"3", "name":"Mr. T", "MoviesActor":{"id":"55", "movie_id":"1", "actor_id":"3"}}
+        ]},
+        {"Movie":{"id":"2", "director_id":"13", "name":"Natural Born Killers", "year":"2001"}, "Director":{"id":"13", "name":"Mary Harron", "alive":true}, "Actor":[
+            {"id":"1", "name":"Dennis Hopper", "MoviesActor":{"id":"38", "movie_id":"2", "actor_id":"1"}},
+            {"id":"2", "name":"Marlon Brando", "MoviesActor":{"id":"83", "movie_id":"2", "actor_id":"2"}},
+            {"id":"3", "name":"Mr. T", "MoviesActor":{"id":"57", "movie_id":"2", "actor_id":"3"}}
+        ]},
+        {"Movie":{"id":"3", "director_id":"13", "name":"Slumdog Millionaire", "year":"2003"}, "Director":{"id":"13", "name":"Mary Harron", "alive":true}, "Actor":[
+            {"id":"1", "name":"Dennis Hopper", "MoviesActor":{"id":"66", "movie_id":"3", "actor_id":"1"}},
+            {"id":"2", "name":"Marlon Brando", "MoviesActor":{"id":"84", "movie_id":"3", "actor_id":"2"}}
+        ]},
+        {"Movie":{"id":"4", "director_id":"12", "name":"Batman - The Dark Knight", "year":"2008"}, "Director":{"id":"12", "name":"Christopher Nolan", "alive":true}, "Actor":[
+            {"id":"9", "name":"Christian Bale", "MoviesActor":{"id":"76", "movie_id":"4", "actor_id":"9"}},
+            {"id":"10", "name":"Heath Ledger", "MoviesActor":{"id":"77", "movie_id":"4", "actor_id":"10"}}
+        ]},
+        {"Movie":{"id":"5", "director_id":"13", "name":"American Psycho", "year":"2000"}, "Director":{"id":"13", "name":"Mary Harron", "alive":true}, "Actor":[
+            {"id":"9", "name":"Christian Bale", "MoviesActor":{"id":"78", "movie_id":"5", "actor_id":"9"}},
+            {"id":"13", "name":"Josh Lucas", "MoviesActor":{"id":"81", "movie_id":"5", "actor_id":"13"}}
+        ]},
+        {"Movie":{"id":"6", "director_id":"12", "name":"Youth Without Youth", "year":"2008"}, "Director":{"id":"12", "name":"Christopher Nolan", "alive":true}, "Actor":[
+            {"id":"11", "name":"Tim Roth", "MoviesActor":{"id":"133", "movie_id":"6", "actor_id":"11"}},
+            {"id":"12", "name":"Bruno Glanz", "MoviesActor":{"id":"123", "movie_id":"6", "actor_id":"12"}}
+        ]},
+        {"Movie":{"id":"7", "director_id":"1", "name":"The Godfather", "year":"1972"}, "Director":{"id":"1", "name":"Francis Ford Coppola", "alive":true}, "Actor":[
+            {"id":"2", "name":"Marlon Brando", "MoviesActor":{"id":"119", "movie_id":"7", "actor_id":"2"}},
+            {"id":"14", "name":"Al Pacino", "MoviesActor":{"id":"120", "movie_id":"7", "actor_id":"14"}}
+        ]},
+        {"Movie":{"id":"8", "director_id":"1", "name":"The Godfather II", "year":"1975"}, "Director":{"id":"1", "name":"Francis Ford Coppola", "alive":true}, "Actor":[
+            {"id":"14", "name":"Al Pacino", "MoviesActor":{"id":"126", "movie_id":"8", "actor_id":"14"}},
+            {"id":"15", "name":"Robert De Niro", "MoviesActor":{"id":"127", "movie_id":"8", "actor_id":"15"}}
+        ]},
+        {"Movie":{"id":"9", "director_id":"13", "name":"The Godfather III", "year":"1990"}, "Director":{"id":"13", "name":"Mary Harron", "alive":true}, "Actor":[
+            {"id":"14", "name":"Al Pacino", "MoviesActor":{"id":"128", "movie_id":"9", "actor_id":"14"}},
+            {"id":"16", "name":"Andy Garcia", "MoviesActor":{"id":"129", "movie_id":"9", "actor_id":"16"}}
+        ]},
+        {"Movie":{"id":"10", "director_id":"1", "name":"Bram Stoker\u00b4s Dracula", "year":"1992"}, "Director":{"id":"1", "name":"Francis Ford Coppola", "alive":true}, "Actor":[
+            {"id":"20", "name":"Keanu Reeves", "MoviesActor":{"id":"118", "movie_id":"10", "actor_id":"20"}}
+        ]},
+        {"Movie":{"id":"11", "director_id":"2", "name":"The Doors", "year":"1991"}, "Director":{"id":"2", "name":"Oliver Stone", "alive":true}, "Actor":[
+            {"id":"17", "name":"Val Kilmer", "MoviesActor":{"id":"91", "movie_id":"11", "actor_id":"17"}},
+            {"id":"18", "name":"Meg Ryan", "MoviesActor":{"id":"92", "movie_id":"11", "actor_id":"18"}}
+        ]},
+        {"Movie":{"id":"12", "director_id":"2", "name":"Born on the Fourth of July", "year":"1989"}, "Director":{"id":"2", "name":"Oliver Stone", "alive":true}, "Actor":[
+            {"id":"19", "name":"Tom Cruise", "MoviesActor":{"id":"121", "movie_id":"12", "actor_id":"19"}}
+        ]},
+        {"Movie":{"id":"13", "director_id":"2", "name":"Any Given Sunday", "year":"1999"}, "Director":{"id":"2", "name":"Oliver Stone", "alive":true}, "Actor":[
+            {"id":"14", "name":"Al Pacino", "MoviesActor":{"id":"124", "movie_id":"13", "actor_id":"14"}},
+            {"id":"22", "name":"Cameron Diaz", "MoviesActor":{"id":"125", "movie_id":"13", "actor_id":"22"}}
+        ]},
+        {"Movie":{"id":"14", "director_id":"5", "name":"28 Days Later", "year":"2002"}, "Director":{"id":"5", "name":"Danny Boyle", "alive":true}, "Actor":[
+            {"id":"21", "name":"Naomie Harris", "MoviesActor":{"id":"96", "movie_id":"14", "actor_id":"21"}}
+        ]},
+        {"Movie":{"id":"15", "director_id":"5", "name":"The Beach", "year":"2000"}, "Director":{"id":"5", "name":"Danny Boyle", "alive":true}, "Actor":[
+            {"id":"23", "name":"Leonardo DiCaprio", "MoviesActor":{"id":"102", "movie_id":"15", "actor_id":"23"}}
+        ]},
+        {"Movie":{"id":"16", "director_id":"5", "name":"Trainspotting", "year":"1996"}, "Director":{"id":"5", "name":"Danny Boyle", "alive":true}, "Actor":[
+            {"id":"24", "name":"Ewan McGregor", "MoviesActor":{"id":"103", "movie_id":"16", "actor_id":"24"}}
+        ]},
+        {"Movie":{"id":"17", "director_id":"6", "name":"Death Proof", "year":"2007"}, "Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}, "Actor":[
+            {"id":"25", "name":"Kurt Russel", "MoviesActor":{"id":"104", "movie_id":"17", "actor_id":"25"}}
+        ]},
+        {"Movie":{"id":"18", "director_id":"6", "name":"Grindhouse", "year":"2007"}, "Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}, "Actor":[
+            {"id":"26", "name":"Bruce Willis", "MoviesActor":{"id":"117", "movie_id":"18", "actor_id":"26"}}
+        ]},
+        {"Movie":{"id":"19", "director_id":"6", "name":"Inglourious Basterds", "year":"2009"}, "Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}, "Actor":[
+            {"id":"27", "name":"Brad Pitt", "MoviesActor":{"id":"106", "movie_id":"19", "actor_id":"27"}}
+        ]},
+        {"Movie":{"id":"20", "director_id":"2", "name":"Platoon2", "year":"1996"}, "Director":{"id":"2", "name":"Oliver Stone", "alive":true}, "Actor":[
+            {"id":"28", "name":"Willem Dafoe", "MoviesActor":{"id":"107", "movie_id":"20", "actor_id":"28"}},
+            {"id":"29", "name":"Charlie Sheen", "MoviesActor":{"id":"108", "movie_id":"20", "actor_id":"29"}}
+        ]},
+        {"Movie":{"id":"21", "director_id":"6", "name":"Kill Bill", "year":"2003"}, "Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}, "Actor":[
+            {"id":"30", "name":"Uma Thurman", "MoviesActor":{"id":"131", "movie_id":"21", "actor_id":"30"}}
+        ]},
+        {"Movie":{"id":"22", "director_id":"6", "name":"Jackie Brown", "year":"1997"}, "Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}, "Actor":[
+            {"id":"31", "name":"Samuel Jackson", "MoviesActor":{"id":"115", "movie_id":"22", "actor_id":"31"}}
+        ]},
+        {"Movie":{"id":"23", "director_id":"6", "name":"Four Rooms", "year":"1996"}, "Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}, "Actor":[
+            {"id":"11", "name":"Tim Roth", "MoviesActor":{"id":"134", "movie_id":"23", "actor_id":"11"}},
+            {"id":"32", "name":"Tim Roth", "MoviesActor":{"id":"111", "movie_id":"23", "actor_id":"32"}}
+        ]},
+        {"Movie":{"id":"24", "director_id":"6", "name":"Pulp Fiction", "year":"1994"}, "Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}, "Actor":[
+            {"id":"11", "name":"Tim Roth", "MoviesActor":{"id":"135", "movie_id":"24", "actor_id":"11"}},
+            {"id":"26", "name":"Bruce Willis", "MoviesActor":{"id":"116", "movie_id":"24", "actor_id":"26"}},
+            {"id":"30", "name":"Uma Thurman", "MoviesActor":{"id":"132", "movie_id":"24", "actor_id":"30"}},
+            {"id":"31", "name":"Samuel Jackson", "MoviesActor":{"id":"114", "movie_id":"24", "actor_id":"31"}}
+        ]},
+        {"Movie":{"id":"25", "director_id":"6", "name":"Reservoir Dogs", "year":"1991"}, "Director":{"id":"6", "name":"Quentin Tarantino", "alive":true}, "Actor":[
+            {"id":"11", "name":"Tim Roth", "MoviesActor":{"id":"136", "movie_id":"25", "actor_id":"11"}},
+            {"id":"32", "name":"Tim Roth", "MoviesActor":{"id":"130", "movie_id":"25", "actor_id":"32"}}
+        ]}
+    ], "total":25}}
+]
